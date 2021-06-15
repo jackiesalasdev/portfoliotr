9e6d65fe50166c7240733d7390f8df5da15b3782
Jackie <jackiesalasdev@gmail.com>
2021-06-15 19:54:58 +0800

    added js

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburgerContainer = document.querySelector("#header-main-nav");
+const hamburger = document.querySelector(".header-main-nav--hamburger");
+const links = document.querySelectorAll(".header-main-nav-links li");
+
+hamburger.addEventListener("click", () => {
+  hamburgerContainer.classList.toggle("clicked");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
